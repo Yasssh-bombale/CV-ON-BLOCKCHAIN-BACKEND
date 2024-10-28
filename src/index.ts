ee1f8acfbf457c2e7f0652f3aba618ffc144b8c3
Yash Vijay Bombale <yashbombale@gmail.com>
2024-10-28 12:53:19 +0530

    checkup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { config } from "dotenv";
 import { MongoConnection } from "./database/mongo.connection";
 import cvRouter from "./routers/cv.router";
@@ -20,6 +20,12 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/cv", cvRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({
+    message: "Health is ok !",
+  });
+});
 
 app.listen(process.env.PORT, () => {
   MongoConnection();
